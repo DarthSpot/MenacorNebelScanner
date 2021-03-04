394e01792c01eb9c847006adc4805d3493a9b80e
Alexander Mills <alexander.mills@akra.de>
2021-03-04 15:16:47 +0100

    Menu and Layout

--- MenacorNebel.App/Views/MainWindow.axaml.cs
+++ MenacorNebel.App/Views/MainWindow.axaml.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace MenacorNebel.App.Views
@@ -16,6 +21,23 @@ namespace MenacorNebel.App.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void ClickCloseApp(object? sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ClickOpenFile(object? sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Title = "Öffne Menacor Datei";
+            ofd.Filters.Add(new FileDialogFilter()
+                {Extensions = new List<string>() {"*.menacor"}, Name = "Menacor Datei"});
+
+            var result = ofd.ShowAsync(this);
+            var path = result.Result.SingleOrDefault();
+            
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MenacorNebelScanner
+
+## Einleitung
+
+Dieses Projekt dient der automatisierten Auswertung von Forenabenteuern auf DSAForum.de. Bestandteile sind zum einen die Scannereinheit, ein Webcrawler, welcher die Daten aus dem Forum ausliest, als auch eine plattformunabhängige App, die diese Daten auswertet.
